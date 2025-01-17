c9d070229a6abf08b79642a3f124b04ff48a6b90
J-Li <98999569+dennis112999@users.noreply.github.com>
2025-01-17 14:00:17 +0900

    [Tilemap] Create TilemapController

---
+++ Assets/Scripts/TileMap/TilemapController.cs
@@ -0,0 +1,51 @@
+using DG.Data;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace DG.Gameplay
+{
+    public class TilemapController : MonoBehaviour
+    {
+        [Header("TileMap")]
+        [SerializeField] private Tilemap _floorTileMap;
+        [SerializeField] private Tilemap _wallTileMap; 
+
+        [Header("Data")]
+        [SerializeField] private TileDataSO _tileDataSO;
+
+        public void PaintFloorTiles(IEnumerable<Vector2Int> floorPos)
+        {
+            PaintTiles(floorPos, _floorTileMap, _tileDataSO.Floor);
+        }
+
+        private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+        {
+            foreach (var pos in positions)
+            {
+                PaintSingleTile(tilemap, tile, pos);
+            }
+        }
+
+        public void Clear()
+        {
+            _floorTileMap.ClearAllTiles();
+        }
+
+        #region Tile
+
+        /// <summary>
+        /// Paint Single Tile
+        /// </summary>
+        /// <param name="tilemap"></param>
+        /// <param name="tile"></param>
+        /// <param name="pos"></param>
+        private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int pos)
+        {
+            var tilePos = tilemap.WorldToCell((Vector3Int)pos);
+            tilemap.SetTile(tilePos, tile);
+        }
+
+        #endregion Tile
+    }
+}
