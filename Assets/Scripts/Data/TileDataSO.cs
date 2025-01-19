003cc666beab904bf0f805a6c07354a9a7b50560
J-Li <98999569+dennis112999@users.noreply.github.com>
2025-01-19 15:31:39 +0900

    [Data] TileDataSO - Add Corner and Island tiles

--- Assets/Scripts/Data/TileDataSO.cs
+++ Assets/Scripts/Data/TileDataSO.cs
@@ -8,6 +8,32 @@ namespace DG.Data
     {
         [Header("Tile Data")]
         public Tile Floor;
-        public Tile Wall;
+
+        [Header("Wall Data")]
+        public Tile Wall_Top;
+        public Tile Wall_SideRight;
+        public Tile Wall_Bottom;
+        public Tile Wall_SideLeft;
+
+        public Tile Wall_InnerConerDownLeft;
+        public Tile Wall_InnerConerDownRight;
+        public Tile Wall_InnerConerUpLeft;
+        public Tile Wall_InnerConerUpRight;
+
+        public Tile Wall_DiagonaCornerDownLeft;
+        public Tile Wall_DiagonaCornerDownRight;
+        public Tile Wall_DiagonaCornerUpLeft;
+        public Tile Wall_DiagonaCornerUpRight;
+
+        public Tile Wall_SideFull;
+
+        [Header("Island")]
+        public Tile Wall_IslandHorizontalLeft; 
+        public Tile Wall_IslandHorizontalMiddle;
+        public Tile Wall_IslandHorizontalRight;
+
+        public Tile Wall_IslandVerticalTop;
+        public Tile Wall_IslandVerticalMiddle;
+        public Tile Wall_IslandVerticalBottom;
     }
 }
--- Assets/Scripts/Editor/TileDataSOEditor.cs
+++ Assets/Scripts/Editor/TileDataSOEditor.cs
@@ -10,19 +10,128 @@ namespace DG.Data
     [CustomEditor(typeof(TileDataSO))]
     public class TileDataSOEditor : Editor
     {
+        private GUIStyle _titleStyle;
+        private GUIStyle _subTitleStyle;
         private GUIStyle _labelStyle;
-        private static int Space = 5;
+
+        private static int Space = 10;
         public override void OnInspectorGUI()
         {
             TileDataSO tileData = (TileDataSO)target;
 
-            EditorGUILayout.LabelField("Tile Data Configuration", EditorStyles.boldLabel);
+            if (_titleStyle == null)
+            {
+                _titleStyle = new GUIStyle(EditorStyles.boldLabel)
+                {
+                    fontSize = 16,
+                    alignment = TextAnchor.MiddleCenter
+                };
+            }
+
+            if (_subTitleStyle == null)
+            {
+                _subTitleStyle = new GUIStyle(EditorStyles.boldLabel)
+                {
+                    fontSize = 16,
+                    alignment = TextAnchor.MiddleLeft
+                };
+            }
+
+            // Title
+            EditorGUILayout.LabelField("Tile Data Configuration", _titleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
             GUILayout.Space(10);
 
-            DrawTileRow("Wall", ref tileData.Wall);
-            GUILayout.Space(Space);
-            DrawTileRow("Floor", ref tileData.Floor);
+            // Floor Tiles Header
+            EditorGUILayout.LabelField("Floor Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
+            // Floor Tile
+            DrawTileRow("Floor", ref tileData.Floor);
+            GUILayout.Space(10);
+
+            // Wall Tiles Header
+            EditorGUILayout.LabelField("Wall Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            // Wall Tiles
+            EditorGUILayout.BeginVertical();
+            DrawTileRow("Wall_Top", ref tileData.Wall_Top);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_SideRight", ref tileData.Wall_SideRight);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_Bottom", ref tileData.Wall_Bottom);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_SideLeft", ref tileData.Wall_SideLeft);
+            EditorGUILayout.EndVertical();
+
+            GUILayout.Space(10);
+
+            // Inner Corner Tiles Header
+            EditorGUILayout.LabelField("Inner Corner Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            // Inner Corner Tiles
+            EditorGUILayout.BeginVertical();
+            DrawTileRow("Wall_InnerConerDownLeft", ref tileData.Wall_InnerConerDownLeft);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_InnerConerDownRight", ref tileData.Wall_InnerConerDownRight);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_InnerConerUpLeft", ref tileData.Wall_InnerConerUpLeft);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_InnerConerUpRight", ref tileData.Wall_InnerConerUpRight);
+            EditorGUILayout.EndVertical();
+
+            GUILayout.Space(10);
+
+            // Diagona Corner Tiles Header
+            EditorGUILayout.LabelField("Diagona Corner Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            // Diagonal Corner Tiles
+            EditorGUILayout.BeginVertical();
+            DrawTileRow("Wall_DiagonaCornerDownLeft", ref tileData.Wall_DiagonaCornerDownLeft);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_DiagonaCornerDownRight", ref tileData.Wall_DiagonaCornerDownRight);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_DiagonaCornerUpLeft", ref tileData.Wall_DiagonaCornerUpLeft);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_DiagonaCornerUpRight", ref tileData.Wall_DiagonaCornerUpRight);
+            EditorGUILayout.EndVertical();
+
+            GUILayout.Space(10);
+
+            // Island - Horizontal Tiles Header
+            EditorGUILayout.LabelField("Island - Horizontal Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            DrawTileRow("Wall_IslandHorizontalLeft", ref tileData.Wall_IslandHorizontalLeft);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_IslandHorizontalMiddle", ref tileData.Wall_IslandHorizontalMiddle);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_IslandHorizontalRight", ref tileData.Wall_IslandHorizontalRight);
+
+            GUILayout.Space(10);
+
+            // Island - Vertical Tiles Header
+            EditorGUILayout.LabelField("Island - Vertical Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            DrawTileRow("Wall_IslandVerticalTop", ref tileData.Wall_IslandVerticalTop);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_IslandVerticalMiddle", ref tileData.Wall_IslandVerticalMiddle);
+            GUILayout.Space(Space);
+            DrawTileRow("Wall_IslandVerticalBottom", ref tileData.Wall_IslandVerticalBottom);
+
+            GUILayout.Space(10);
+
+            // Others Wall Tiles Header
+            EditorGUILayout.LabelField("Other Wall Tiles", _subTitleStyle);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            DrawTileRow("Wall_SideFull", ref tileData.Wall_SideFull);
+
+            // Mark as dirty if GUI changed
             if (GUI.changed)
             {
                 EditorUtility.SetDirty(tileData);
@@ -41,20 +150,21 @@ namespace DG.Data
 
             EditorGUILayout.BeginHorizontal();
 
+            // Label and Tile setting
             GUILayout.BeginVertical(GUILayout.Width(300));
-
-            // Tile setting
             EditorGUILayout.LabelField(label, _labelStyle);
             tile = (Tile)EditorGUILayout.ObjectField(tile, typeof(Tile), false);
-
             GUILayout.EndVertical();
 
-            // tile sprite
+            // Add flexible space to push the sprite to the right
+            GUILayout.FlexibleSpace();
+
+            // Tile sprite preview
             GUILayout.BeginVertical(GUILayout.Width(60));
+            GUILayout.FlexibleSpace();
+            Rect rect = GUILayoutUtility.GetRect(50, 50, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
             if (tile != null && tile.sprite != null)
             {
-                GUILayout.FlexibleSpace();
-                Rect rect = GUILayoutUtility.GetRect(50, 50, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
                 Texture texture = AssetPreview.GetAssetPreview(tile.sprite);
                 if (texture != null)
                 {
@@ -65,20 +175,18 @@ namespace DG.Data
                     EditorGUI.DrawRect(rect, Color.white);
                     GUI.Label(rect, "Sprite Missing", new GUIStyle { alignment = TextAnchor.MiddleCenter });
                 }
-                GUILayout.FlexibleSpace();
             }
             else
             {
-                GUILayout.FlexibleSpace();
-                Rect rect = GUILayoutUtility.GetRect(50, 50, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
                 EditorGUI.DrawRect(rect, Color.gray);
                 GUI.Label(rect, "No Tile", new GUIStyle { alignment = TextAnchor.MiddleCenter });
-                GUILayout.FlexibleSpace();
             }
+            GUILayout.FlexibleSpace();
             GUILayout.EndVertical();
 
             EditorGUILayout.EndHorizontal();
         }
+
     }
 
 }
